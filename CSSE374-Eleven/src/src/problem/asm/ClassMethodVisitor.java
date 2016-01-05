28f5bd4d27add36bb57bb3ddfa1771c41721c4a6
Shayna Oriold <soriold02@gmail.com>
2016-01-04 20:26:34 -0500

    Added implementation to ClassMethodVisitor

--- CSSE374-Eleven/src/src/problem/asm/ClassMethodVisitor.java
+++ CSSE374-Eleven/src/src/problem/asm/ClassMethodVisitor.java
@@ -5,33 +5,45 @@ import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 
+import src.problem.components.IClass;
+import src.problem.components.IMethod;
+import src.problem.components.IParameter;
+import src.problem.components.Method;
+import src.problem.components.Parameter;
+
 public class ClassMethodVisitor extends ClassVisitor {
-	public ClassMethodVisitor(int api) {
+	
+	private IClass clazz;
+	
+	public ClassMethodVisitor(int api, IClass clazz) {
 		super(api);
+		this.clazz = clazz;
 	}
 
-	public ClassMethodVisitor(int api, ClassVisitor decorated) {
+	public ClassMethodVisitor(int api, ClassVisitor decorated, IClass clazz) {
 		super(api, decorated);
+		this.clazz = clazz;
 	}
 
 	@Override
 	public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
 		MethodVisitor toDecorate = super.visitMethod(access, name, desc, signature, exceptions);
-		// TODO: delete the line below
-		System.out.println("method " + name);
-		// TODO: create an internal representation of the current method and
+		//System.out.println("method " + name);
+		// DONE: create an internal representation of the current method and
 		// pass it to the methods below
-		addAccessLevel(access);
-		addReturnType(desc);
-		addArguments(desc);
-		// TODO: add the current method to your internal representation of the
+		IMethod method = new Method();
+		method.setName(name);
+		
+		addAccessLevel(access, method);
+		addReturnType(desc, method);
+		addArguments(desc, method);
+		// DONE: add the current method to your internal representation of the
 		// current class
-		// What is a good way for the code to remember what the current class
-		// is?
+		this.clazz.addMethod(method);
 		return toDecorate;
 	}
 
-	void addAccessLevel(int access) {
+	void addAccessLevel(int access, IMethod method) {
 		String level = "";
 		if ((access & Opcodes.ACC_PUBLIC) != 0) {
 			level = "public";
@@ -42,28 +54,31 @@ public class ClassMethodVisitor extends ClassVisitor {
 		} else {
 			level = "default";
 		}
-		// TODO: delete the next line
-		System.out.println("access level: " + level);
-		// TODO: ADD this information to your representation of the current
+		
+		//System.out.println("access level: " + level);
+		//DONE: ADD this information to your representation of the current
 		// method.
+		method.addModifier(level);
 	}
 
-	void addReturnType(String desc) {
+	void addReturnType(String desc, IMethod method) {
 		String returnType = Type.getReturnType(desc).getClassName();
-		// TODO: delete the next line
-		System.out.println("return type: " + returnType);
-		// TODO: ADD this information to your representation of the current
+		//System.out.println("return type: " + returnType);
+		// DONE: ADD this information to your representation of the current
 		// method.
+		method.setReturnType(returnType);
 	}
 
-	void addArguments(String desc) {
+	void addArguments(String desc, IMethod method) {
 		Type[] args = Type.getArgumentTypes(desc);
 		for (int i = 0; i < args.length; i++) {
 			String arg = args[i].getClassName();
-			// TODO: delete the next line
-			System.out.println("arg " + i + ": " + arg);
-			// TODO: ADD this information to your representation of the current
+			//System.out.println("arg " + i + ": " + arg);
+			// DONE: ADD this information to your representation of the current
 			// method.
+			IParameter parameter = new Parameter();
+			parameter.setType(arg);
+			method.addParameter(parameter);
 		}
 	}
 }
