244ac4b889f80ed7bd84c90a0fd436ac776a2577
taylerhow <taylerhow@gmail.com>
2016-01-14 15:15:23 -0500

    updated integration tests

--- CSSE374-Eleven/src/integrationTests/SDEOSIntegrationTests.java
+++ CSSE374-Eleven/src/integrationTests/SDEOSIntegrationTests.java
@@ -11,7 +11,6 @@ import src.problem.asm.DesignParser;
 import src.problem.components.IClass;
 import src.problem.components.IModel;
 import src.problem.components.Model;
-import src.problem.outputvisitor.GraphVizOutputStream;
 import src.problem.outputvisitor.SDEditOutputStream;
 
 public class SDEOSIntegrationTests {
@@ -32,7 +31,7 @@ public class SDEOSIntegrationTests {
 	}
 
 	@Test
-	public void testHasMethodCalls() throws IOException {
+	public void testHasMethodCallsDepth2() throws IOException {
 		IModel model = new Model();
 		IClass clazz = DesignParser.parse("java.util.Collections", model);
 		model.addClass(clazz);
@@ -46,5 +45,23 @@ public class SDEOSIntegrationTests {
 		assertTrue(result.contains(
 				"Collections:void=Random.create()\nCollections:void=Collections.shuffle(List Random )\nCollections:int=List.size()\nCollections:int=Random.nextInt(I )\nCollections:void=Collections.swap(List I I )\nCollections:Object=List.toArray()\nCollections:int=Random.nextInt(I )\nCollections:void=Collections.swap(Object I I )\nCollections:ListIterator=List.listIterator()\nCollections:Object=ListIterator.next()\nCollections:void=ListIterator.set(Object )"));
 	}
+	
+	@Test
+	public void testHasMethodCallsDepth3() throws IOException {
+		IModel model = new Model();
+		IClass clazz = DesignParser.parse("java.util.Collections", model);
+		model.addClass(clazz);
+
+		ByteArrayOutputStream resultStream = new ByteArrayOutputStream();
+		SDEditOutputStream sdeos = new SDEditOutputStream(resultStream);
+		sdeos.writeMethod((Model) model, "java.util.Collections.shuffle(List<T> list)", 3);
+		sdeos.close();
+		String result = resultStream.toString();
+
+		assertTrue(result.contains(
+				"Collections:void=Random.create()\nCollections:void=Collections.shuffle(List Random )\nCollections:int=List.size()\nCollections:int=Random.nextInt(I )\nCollections:void=Collections.swap(List I I )\nCollections:Object=List.get(I )\nCollections:Object=List.set(I Object )\nCollections:Object=List.set(I Object )\nCollections:Object=List.toArray()\nCollections:int=Random.nextInt(I )\nCollections:void=Collections.swap(Object I I )\nCollections:Object=List.get(I )\nCollections:Object=List.set(I Object )\nCollections:Object=List.set(I Object )\nCollections:ListIterator=List.listIterator()\nCollections:Object=ListIterator.next()\nCollections:void=ListIterator.set(Object )"));
+	}
+	
+	
 
 }
