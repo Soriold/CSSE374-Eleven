8c88582bd16c3183a9f5d1a0cb075f98a1cc8b37
Shayna Oriold <soriold02@gmail.com>
2016-02-14 16:08:15 -0500

    Merge remote-tracking branch 'refs/remotes/origin/master' into GUI
    # Conflicts:
    #	GUI/Parser.jar

---
+++ GUI/src/GUI/ClassListPanel.java
@@ -0,0 +1,36 @@
+package GUI;
+
+
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.ListModel;
+
+import src.problem.components.IClass;
+import src.problem.components.Model;
+
+public class ClassListPanel extends JScrollPane {
+	
+	private List<IClass> classes;
+	
+	public ClassListPanel(Model m) {
+		
+		JList list = new JList();
+		DefaultListModel model = new DefaultListModel();
+		for(IClass c : m.getClasses()) {
+			model.addElement(c.getName());
+		}
+		add(list);
+		list.setMinimumSize(new Dimension(this.getWidth(), this.getHeight()));
+		//list.setSize(new Dimension(this.getWidth(), this.getHeight()));
+		
+		
+		classes = m.getClasses();
+	}
+
+}
